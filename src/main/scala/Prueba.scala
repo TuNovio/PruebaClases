624065e315ab37a3ba5e11e9ede950b612f28b1c
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-08 16:51:03 -0500

    first commit

---
+++ src/main/scala/Prueba.scala
@@ -0,0 +1,34 @@
+/*
+para que sea igual que en java se inicia como metodo
+los atributos de la clase se pasan como parametros
+se inicializan las propiedades de la clase
+*/
+class Alumno(nombre:String, apellido:String, edad:Int){
+    /*
+    */
+    def visualizar()={
+        println("Nombre: " + nombre)
+        println("Apellido: " + apellido)
+        println("Edad: " + edad)
+    }
+    /*
+    */
+    def asignarVehiculo(nTipo:String, nCosto:Int)={
+        var mv = new Vehiculo(tipo = nTipo, costo = nCosto)
+        print(s"Vehiculo asignado: $nTipo con un costo de: $nCosto")
+    }
+}
+/*
+los atributos de la clase se inicializan en el objeto que se crea
+nombre es el nombre del alumno 
+apellido es el apellido del alumno 
+edad es la edad del alumno
+*/
+object Principal{
+
+    def main(args: Array[String]): Unit={
+        var miAlumno = new Alumno(nombre = "Alfonso", apellido = "Gomajoa", edad = 21)
+        var asignado = miAlumno.asignarVehiculo(nTipo = "Carro particular", nCosto = 500)
+        println(asignado)
+    }   
+}
---
+++ src/main/scala/Vehiculo.scala
@@ -0,0 +1,18 @@
+class Vehiculo(var tipo:String, var costo:Int){
+    def darTipo={
+        println(tipo)
+    }
+    def darCosto={
+        println(costo)
+    }
+    def visualizarV={
+        println(s"El tipo de carro es: $tipo con un costo de $costo")
+    }    
+}
+
+object miVehiculo{
+    def main(args: Array[String])={
+        var mv = new Vehiculo(tipo = "Bus", costo = 120)
+        print(mv.darTipo)
+    }   
+}
